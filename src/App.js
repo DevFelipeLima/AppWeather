cb748aaa70eebaafd60b14e32092fcaeb7965b68
DevFelipeLima <felipelimapsn1717@gmail.com>
2022-04-19 10:36:52 -0300

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+import './index.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false)
+  const [weather, setWeather] = useState(false)
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    })
+    setWeather(res.data)
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude)
+      setLocation(true)
+    })
+  }, [])
+  if (location === false) {
+    return (
+      <div className="ErrorMensage">
+        Você precisa dar permissão para acessar sua localização
+      </div>
+    )
+  } else {
+    return (
+      <div className="Exhibition">
+        <h1>Clima em {weather['name']}: {weather['weather'][0]['description']}</h1>
+        <hr />
+        <div className="informations">
+
+          <spam>Temperatura Atual: {weather['main']['temp']}°</spam>
+          <spam>Sensação Termica: {weather['main']['feels_like']}°</spam>
+          <spam>Temperatura Maxima: {weather['main']['temp_max']}°</spam>
+          <spam>Temperatura Minima: {weather['main']['temp_min']}°</spam>
+          <spam>Umidade relativa: {weather['main']['humidity']}%</spam>
+          <spam>Vento: {weather['wind']['speed']}km/h</spam>
+          <spam>Rajadas: {weather['wind']['gust']}km/h</spam>
+
+        </div>
+
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
